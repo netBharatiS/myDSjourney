c94aa4af16ad8f34a5ac0242996024a333fa0aa2
netBharatiS <153512719+netBharatiS@users.noreply.github.com>
2024-03-13 17:01:50 +0000

    Create conversion.py

---
+++ conversion.py
@@ -0,0 +1,21 @@
+
+### declare variables with defaults
+# convert variable types using Casting as per requirement
+# num1 to integer
+# num2 to float
+# num3 to string
+# string1 into integer
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+
+print(int(num1))
+
+print(float(num2))
+
+print(str(num3))
+
+print(int(string1))
